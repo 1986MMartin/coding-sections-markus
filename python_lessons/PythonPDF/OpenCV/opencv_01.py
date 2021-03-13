8b3c56370255e2f40ec2da7c06eda48fe28cd740
Markus Martin <1986mmartin@gmail.com>
2021-03-14 00:14:54 +0100

    Aenderung in OpenCV

--- python_lessons/PythonPDF/OpenCV/opencv_01.py
+++ python_lessons/PythonPDF/OpenCV/opencv_01.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 
-image=cv2.imread('Foto2.jpeg')
+image=cv2.imread("Foto2.jpg")
 image=cv2.resize(image,(1300,800))
 orig=image.copy()
 
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-cv2.imshow('Title',gray)
+cv2.imshow("Title",gray)
