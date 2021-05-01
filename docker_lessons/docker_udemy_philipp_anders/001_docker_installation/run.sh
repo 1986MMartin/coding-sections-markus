92448e71074e05a088bca3472ba6ace9914dac29
Markus Martin <1986mmartin@gmail.com>
2021-05-01 23:21:55 +0200

    new Docker runs file

--- docker_lessons/docker_udemy_philipp_anders/001_docker_installation/run.sh
+++ docker_lessons/docker_udemy_philipp_anders/001_docker_installation/run.sh
@@ -4,7 +4,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -17,3 +17,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
