380cfeb89a8e6c5f13ef66ceff3db9715a547b02
Markus Martin <1986mmartin@gmail.com>
2021-05-01 20:38:48 +0200

    new Python Udemy Lesson

--- python_lessons/udemy_lessons/the_Complete_Python_Course/054_Milestone_1_Movie_Database.py
+++ python_lessons/udemy_lessons/the_Complete_Python_Course/054_Milestone_1_Movie_Database.py
@@ -1,7 +1,6 @@
 MENU_PROMPT = "/nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
 movies = []
 
-
 def add_movie():
     title = input("Enter the movie title: ")
     director = input("Enter the movie director: ")
@@ -29,15 +28,18 @@ def find_movie():
         if movie["title"] == search_title:
             print_movie(movie)
 
+user_options = {
+        "a": add_movie,
+        "l": show_movies,
+        "f": find_movie
+        }
+
 def menu():
     selection = input(MENU_PROMPT)
     while selection != 'q':
-        if selection == 'a':
-            add_movie()
-        elif selection == 'l':
-            show_movies()
-        elif selection == 'f':
-            find_movie()
+        if selection in user_options:
+            selected_function = user_options[selection]
+            selected_function()
         else:
             print ("Unknown command. Please try again.")
 
