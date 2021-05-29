5cd74cce85313b3d4b8e6a79bb538bfbf5e29b3f
Markus Martin <1986mmartin@gmail.com>
2021-05-29 13:49:35 +0200

    new Linux Commands for x2Go Server

---
+++ linux_init/x2go-server/x2go_for_server.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:x2go/stable
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install x2goserver x2goserver-xsession -y
+sudo apt-get update && sudo apt-get upgrade -y
